8f4be30231c5e0c2963cc5708944acea917e1bce
Yahya Mouelhi <101068337+YahyaMouelhi@users.noreply.github.com>
2023-04-10 19:19:29 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector("#btn-socials").addEventListener("click",function(){
+    document.querySelector(".socials").classList.toggle("close-socials");
+    document.querySelector("#btn-socials").classList.toggle("show-btn");
+})
